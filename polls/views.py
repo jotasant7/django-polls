bd488902f4045e4793849f2a0c9bbfd73b0e1a4b
Jônatas de Macedo <jonatas.santos@aluno.ifsp.edu.br>
2023-06-28 09:19:12 -0300

    correção de 2 aulas + html

--- polls/views.py
+++ polls/views.py
@@ -1,18 +1,19 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 from django.http import HttpResponse
+from .models import Question
 
 def index(request):
-    return HttpResponse("Olá Mundo! Esta é a página inicial do aplicativo.")
+    latest_question_list = Question.objects.order_by("-pub_date")[:5]
+    context = {"latest_question_list": latest_question_list}
+    return render(request, "polls/index.html", context)
     
-    def detail(request, question_id):
-        return HttpResponse("You're looking at question %s." % question_id)
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk= question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 
 def results(request, question_id):
-    response = "You're looking at the results of question %s."
+    response = f'resultados da pergunta número {question_id}'
     return HttpResponse(response % question_id)
 
 
